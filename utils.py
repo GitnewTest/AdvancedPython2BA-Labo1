e581e89e58dc2b87714f21555040e7856d264f1c
Mohamad Mroue <mrouemohamad@hotmail.fr>
2017-03-05 19:46:14 +0100

    Fix utils module to pass tests

--- utils.py
+++ utils.py
@@ -2,10 +2,6 @@
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
 
-import math
-from numpy.polynomial import Polynomial
-from scipy.integrate import quad
-
 def fact(n):
     """Computes the factorial of a natural number.
     
@@ -13,7 +9,13 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
-    return math.factorial(n)
+    p = 0
+    z = 1
+    while p < n:
+        z *= n - p
+        p += 1
+    return z
+    
 
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -22,15 +24,17 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    delta = b ** 2 - 4 * a * c
-    if delta > 0:
-        return ( (-b + math.sqrt(delta))/2*a ,  (-b - math.sqrt(delta))/2*a)
-    elif delta == 0:
-        return ( (-b + math.sqrt(delta))/2*a )
+    d = b ** 2 - 4 * a * c
+    if d > 0:
+        root1 = (-b + d ** 0.5) / (2 * a)
+        root2 = (-b - d ** 0.5) / (2 * a)
+        return (root1, root2)
+    elif d == 0:
+        root = -b / (2 * a)
+        return (root)
     else:
         return ()
 
-    return 
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -41,10 +45,15 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    def integrand(x):
-        return x ** 2 - 1
+    n = 50  #Nombre de pas
+    h = (upper - lower) / n  #Aire des petits rectangles
 
-    return quad(integrand, lower, upper)[0]
+    z = 0
+    for i in range(n):
+        x = lower + (i + 0.5) * h
+        z += eval(function) * h
+    return round(z,3)
+
 
 if __name__ == '__main__':
     print(fact(5))
